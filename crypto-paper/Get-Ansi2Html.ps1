e1cd31ea99705a401df9c224d61e828df4322c3c
Gee Law <geelaw@outlook.com>
2024-04-08 15:05:33 -0700

    style(crypto-paper): improve Get-Ansi2Html.ps1 style

--- crypto-paper/Get-Ansi2Html.ps1
+++ crypto-paper/Get-Ansi2Html.ps1
@@ -1,9 +1,8 @@
 [CmdletBinding()]
 Param()
 
-Process
+End
 {
-  <# Download ansi2html.sh. #>
-  $ansi2html = [System.IO.Path]::Combine($PSScriptRoot, 'ansi2html.sh');
-  Start-BitsTransfer -Source 'https://raw.githubusercontent.com/pixelb/scripts/master/scripts/ansi2html.sh' -Destination $ansi2html;
+  $script:Dest = [System.IO.Path]::Combine($PSScriptRoot, 'ansi2html.sh');
+  Start-BitsTransfer -Source 'https://raw.githubusercontent.com/pixelb/scripts/master/scripts/ansi2html.sh' -Destination $script:Dest;
 }
